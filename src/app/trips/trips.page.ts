5ffb058ccfdab6ce78dfda557047ef34499a2c5c
Jade-Altea perroset <jadeperroset@gmail.com>
2021-12-13 10:53:48 +0100

    Add Logout

--- src/app/trips/trips.page.ts
+++ src/app/trips/trips.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
   selector: 'app-trips',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    // Inject the authentication provider.
+    private auth: AuthService,
+    // Inject the router
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  // Add a method to log out.
+  logOut() {
+    console.log("logging out...");
+    this.auth.logOut();
+    this.router.navigateByUrl("/login");
   }
 
 }
